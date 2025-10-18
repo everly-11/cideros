a51fcc7441b9eaa75175e83fd4aeb70f2ef5d588
Sara <simon.esp1814@gmail.com>
2025-10-18 21:22:07 +0200

    Update customfuncs.py

--- cdrl/customfuncs.py
+++ cdrl/customfuncs.py
@@ -1,7 +1,20 @@
-def func(action, input):
-    if action == "str":
-        return str(input)
-    if action == "int":
-        return int(input)
-    if action == "float":
-        return float(input)
+import kernel
+import ast
+def func(action, inp, args):
+    match action:
+        case "str":
+            return str(inp)
+        case "int":
+            return int(inp)
+        case "float":
+            return float(inp)
+        case "bool":
+            return (inp == "True") if inp in ("True", "False") else bool(inp)
+        case "input":
+            return input(inp)
+        case "var":
+            return kernel.get(inp)
+        case "not":
+            return not (inp == "True") if inp in ("True", "False") else bool(inp)
+        case "arg":
+            return ast.literal_eval(args)[inp]
