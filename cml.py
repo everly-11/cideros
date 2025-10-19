9fcf3b2648d1795e7f652f9269a1d2ea421ef383
Sara <simon.esp1814@gmail.com>
2025-10-19 14:19:23 +0200

    Update cml.py

--- cml.py
+++ cml.py
@@ -1,5 +1,6 @@
 import os
 import expinterpreter
+import cdrl
 curdir = os.getcwd()
 
 def getfile(name):
@@ -8,9 +9,14 @@ def getfile(name):
 
 print("Welcome to Cider OS commandline\nUse ´help´ for a list of commands")
 while True:
-    cmd=input(f"{curdir}> ").split()
+    cmdfull = input(f"{curdir}> ")
+    cmd=cmdfull.split()
     match cmd[0]:
         case "help":
             print(getfile("help.txt"))
         case "echo":
             print(expinterpreter.interpret(" ".join(cmd[1:])))
+        case "install":
+            cdrl.run("install.cdrl", f'["{cmd[1]}"]')
+        case "cdrl":
+            cdrl.run(cmd[1], f'[{cmdfull.split(" ", 2)[2]}]')
