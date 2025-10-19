dd81c4fdf37b0c351fe2d5151bc91e2acee98598
Sara <simon.esp1814@gmail.com>
2025-10-19 18:16:12 +0200

    added more

--- cdrl/customfuncs.py
+++ cdrl/customfuncs.py
@@ -1,5 +1,7 @@
 import kernel
 import ast
+import os
+import expinterpreter
 def func(action, inp, args):
     match action:
         case "str":
@@ -17,6 +19,18 @@ def func(action, inp, args):
         case "not":
             return not (inp == "True") if inp in ("True", "False") else bool(inp)
         case "arg":
-            return ast.literal_eval(args)[inp]
+            try:
+                return ast.literal_eval(args)[inp]
+            except IndexError:
+                print("missing argument")
+                print("attempted to find arg", inp, "in", args)
         case "request":
             return kernel.request(inp)
+        case "getfile":
+            return kernel.getfile(inp)
+        case "oswdraw":
+            return os.getcwd()
+        case "eval":
+            return expinterpreter.interpret(inp, args)
+        case "len":
+            return len(inp)
