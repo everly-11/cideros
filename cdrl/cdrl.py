2806d99f80e2befe6b298ba69cc242aea321ea82
Sara <simon.esp1814@gmail.com>
2025-10-19 18:16:35 +0200

    a bunch of fixes and added some commands

--- cdrl/cdrl.py
+++ cdrl/cdrl.py
@@ -45,37 +45,42 @@ def run(filename,args):
         while not line[0] == "end":
             index += 1
             line = tokenize(code[index], " ")
-            match line[0]:
-                case "echo":
-                    print(expinterpreter.interpret(line[1], args))
-                case "rep":
-                    for i in range(int(expinterpreter.interpret(line[1], args))):     
-                        runlines(tokenize(line[2][1:-1], ";"), args=args)
-                case "while":
-                    while expinterpreter.interpret(line[1], args):     
-                        runlines(tokenize(line[2][1:-1], ";"), args=args)
-                case "wipe":
-                    kernel.wipe()
-                case "def":
-                    kernel.write(line[2], line[1])
-                case "if":
-                    if expinterpreter.interpret(line[1], args):
-                        runlines(tokenize(line[2][1:-1], ";"), args=args)
-                    elif line [3] == "else":
-                        runlines(tokenize(line[4][1:-1], ";"), args=args)
-                case "var":
-                    kernel.write(expinterpreter.interpret(line[2], args), expinterpreter.interpret(line[1], args))
-                case "varadd":
-                    kernel.write(expinterpreter.interpret(f'("var":{line[1]})', args) + expinterpreter.interpret(line[2], args), expinterpreter.interpret(line[1], args))
-                case "call":
-                    runlines(tokenize(expinterpreter.interpret(f'("var":"{line[1]}")', args)[1:-1], ";"), expinterpreter.interpret(line[2], args))
-                case "append":
-                    arr = ast.literal_eval(expinterpreter.interpret(f'("var":{line[1]})', args))
-                    arr.append(expinterpreter.interpret(line[2], args))
-                    kernel.write(str(arr), expinterpreter.interpret(line[1], args))
-                case "writefile":
-                    with open(expinterpreter.interpret(line[1], args), "w") as f:
-                        f.write(expinterpreter.interpret(line[2], args))
+            if not line[0][0:2] == "//":
+                match line[0]:
+                    case "echo":
+                        print(expinterpreter.interpret(line[1], args))
+                    case "rep":
+                        for i in range(int(expinterpreter.interpret(line[1], args))):     
+                            runlines(tokenize(line[2][1:-1], ";"), args=args)
+                    case "while":
+                        while expinterpreter.interpret(line[1], args):     
+                            runlines(tokenize(line[2][1:-1], ";"), args=args)
+                    case "wipe":
+                        kernel.wipe()
+                    case "def":
+                        kernel.write(line[2], line[1])
+                    case "if":
+                        if expinterpreter.interpret(line[1], args):
+                            runlines(tokenize(line[2][1:-1], ";"), args=args)
+                        elif len(line) > 3:
+                            if line [3] == "else":
+                                runlines(tokenize(line[4][1:-1], ";"), args=args)
+                    case "var":
+                        kernel.write(expinterpreter.interpret(line[2], args), expinterpreter.interpret(line[1], args))
+                    case "varadd":
+                        kernel.write(expinterpreter.interpret(f'("var":{line[1]})', args) + expinterpreter.interpret(line[2], args), expinterpreter.interpret(line[1], args))
+                    case "call":
+                        runlines(tokenize(expinterpreter.interpret(f'("var":"{line[1]}")', args)[1:-1], ";"), expinterpreter.interpret(line[2], args))
+                    case "append":
+                        arr = ast.literal_eval(expinterpreter.interpret(f'("var":{line[1]})', args))
+                        arr.append(expinterpreter.interpret(line[2], args))
+                        kernel.write(str(arr), expinterpreter.interpret(line[1], args))
+                    case "writefile":
+                        with open(expinterpreter.interpret(line[1], args), "w") as f:
+                            f.write(expinterpreter.interpret(line[2], args))
+                    case "cdrl":
+                        run(expinterpreter.interpret(line[1], args), expinterpreter.interpret(line[2], args))
+
     runlines(file, args)
 
-#run("install.cdrl", '["txt/help.txt"]')
+#run("cml.cdrl", '[]')
