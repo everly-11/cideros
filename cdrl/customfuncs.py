e69b63746472f2ace04eb22495e3bae675c524c6
Sara <simon.esp1814@gmail.com>
2025-10-25 18:29:35 +0200

    add functions and literal eval, fix not

--- cdrl/customfuncs.py
+++ cdrl/customfuncs.py
@@ -2,6 +2,8 @@ import kernel
 import ast
 import os
 import expinterpreter
+import platform
+import cdrl
 def func(action, inp, args):
     match action:
         case "str":
@@ -12,12 +14,14 @@ def func(action, inp, args):
             return float(inp)
         case "bool":
             return (inp == "True") if inp in ("True", "False") else bool(inp)
+        case "liteval":
+            return ast.literal_eval(inp)
         case "input":
             return input(inp)
         case "var":
             return kernel.get(inp)
         case "not":
-            return not (inp == "True") if inp in ("True", "False") else bool(inp)
+            return (not {"True": True, "False": False}.get(str(inp), bool(inp)))
         case "arg":
             try:
                 return ast.literal_eval(args)[inp]
@@ -34,3 +38,7 @@ def func(action, inp, args):
             return expinterpreter.interpret(inp, args)
         case "len":
             return len(inp)
+        case "llsystem":
+            return platform.system()
+        case "call":
+            return cdrl.runlines(cdrl.tokenize(expinterpreter.interpret(f'("var":"{inp}")', args)[1:-1], ";"), args)
